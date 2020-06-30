5a4aab1eb56d26c199e028b3363f0f1307b4b31b
Nikolai <n.poperechnij@gmail.com>
2020-06-30 17:46:03 +0300

    updated version

--- src/main/java/ru/itmo/ctlab/sgmwcs/Main.java
+++ src/main/java/ru/itmo/ctlab/sgmwcs/Main.java
@@ -19,7 +19,7 @@ import java.util.Set;
 import static java.util.Arrays.asList;
 
 public class Main {
-    public static final String VERSION = "0.9.8";
+    public static final String VERSION = "0.9.9";
 
     static {
         /*try {
